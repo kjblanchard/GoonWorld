696fc2ecbc4d26bfe63e738381e23bf27ea7580f
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-01-02 08:59:17 -0500

    Move around player, no physics

--- GoonWorld/objects/Player.cs
+++ GoonWorld/objects/Player.cs
@@ -12,32 +12,50 @@ class Player : GameObject
         Location.X = (int)castedData.x;
         Location.Y = (int)castedData.y;
         _keyboardComponent = new KeyboardComponent();
-        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebA, Models.SDL_Scancode.SDL_SCANCODE_SPACE);
         // _drawComponent = new DrawComponent(_locationComponent) { Width = (int)castedData.width, Height = (int)castedData.height };
         _drawComponent = new DrawComponent(_locationComponent);
         _drawComponent.Width = (int)castedData.width;
         _drawComponent.Height = (int)castedData.height;
         _drawComponent.Width = 44;
         _drawComponent.Height = 50;
+        SetControllerButtons();
         AddComponent(_keyboardComponent, _drawComponent);
+    }
+
+    private void SetControllerButtons()
+    {
+        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebA, Models.SDL_Scancode.SDL_SCANCODE_SPACE);
+        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebLeft, Models.SDL_Scancode.SDL_SCANCODE_A);
+        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebRight, Models.SDL_Scancode.SDL_SCANCODE_D);
+        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebUp, Models.SDL_Scancode.SDL_SCANCODE_W);
+        _keyboardComponent.SetControllerButton(Models.EngineButtons.ebDown, Models.SDL_Scancode.SDL_SCANCODE_S);
+    }
+
+    private void HandleInput()
+    {
+        if(_keyboardComponent.GetButtonDown(Models.EngineButtons.ebLeft))
+        {
+            Location.X -= 1;
+        }
+        if(_keyboardComponent.GetButtonDown(Models.EngineButtons.ebRight))
+        {
+            Location.X += 1;
+        }
+        if(_keyboardComponent.GetButtonDown(Models.EngineButtons.ebUp))
+        {
+            Location.Y -= 1;
+        }
+        if(_keyboardComponent.GetButtonDown(Models.EngineButtons.ebDown))
+        {
+            Location.Y += 1;
+        }
+
     }
 
     public override void Update()
     {
         base.Update();
-        // Console.WriteLine($"I am the player, and my location is {Location.X}:  {Location.Y}!");
-        if (_keyboardComponent.GetButtonPressed(Models.EngineButtons.ebA))
-        {
-            Console.WriteLine("Button just presed!");
-        }
-        if (_keyboardComponent.GetButtonHeld(Models.EngineButtons.ebA))
-        {
-            Console.WriteLine("Button held!");
-        }
-        if (_keyboardComponent.GetButtonReleased(Models.EngineButtons.ebA))
-        {
-            Console.WriteLine("Button released!");
-        }
+        HandleInput();
     }
 
 }
