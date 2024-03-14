645cb05e5efe7b0c56fe28fb88542cf64bb131c8
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-14 10:20:38 -0400

    initial start

--- external/sg_engine/src/GoonEngine/scripting/SdlSurface.c
+++ external/sg_engine/src/GoonEngine/scripting/SdlSurface.c
@@ -1,12 +1,12 @@
+#include <png.h>
 #include <GoonEngine/gnpch.h>
-#include <external/stb_image.h>
 #include <GoonEngine/debug.h>
 #include <GoonEngine/SdlSurface.h>
 #include <GoonEngine/color.h>
 #include <GoonEngine/rectangle.h>
 
 SDL_Texture *g_BackgroundAtlas = NULL;
-SDL_Rect* g_backgroundDrawRect = NULL;
+SDL_Rect *g_backgroundDrawRect = NULL;
 extern SDL_Renderer *g_pRenderer;
 
 void SetBackgroundAtlas(SDL_Texture *background)
@@ -15,7 +15,7 @@ void SetBackgroundAtlas(SDL_Texture *background)
 }
 void SetCameraRect(geRectangle *rect)
 {
-    g_backgroundDrawRect = (SDL_Rect*)rect;
+    g_backgroundDrawRect = (SDL_Rect *)rect;
 }
 
 static void flipImageVertically(unsigned char *data, int width, int height, int channels)
@@ -35,57 +35,189 @@ static void flipImageVertically(unsigned char *data, int width, int height, int 
         }
     }
 }
+static void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+    Uint8 *target_pixel = (Uint8 *)surface->pixels + y * surface->pitch + x * sizeof(Uint32);
+    *(Uint32 *)target_pixel = pixel;
+}
+
+static SDL_Surface *loadPNG(const char *filename)
+{
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Unable to open file %s\n", filename);
+        return NULL;
+    }
+
+    // Check if the file is a PNG file
+    unsigned char header[8];
+    fread(header, 1, 8, file);
+    if (png_sig_cmp(header, 0, 8))
+    {
+        fclose(file);
+        fprintf(stderr, "Error: %s is not a PNG file\n", filename);
+        return NULL;
+    }
+
+    // Initialize libpng structures
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr)
+    {
+        fclose(file);
+        fprintf(stderr, "Error: Unable to create png_struct\n");
+        return NULL;
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr)
+    {
+        fclose(file);
+        png_destroy_read_struct(&png_ptr, NULL, NULL);
+        fprintf(stderr, "Error: Unable to create png_info\n");
+        return NULL;
+    }
+
+    // Set error handling
+    if (setjmp(png_jmpbuf(png_ptr)))
+    {
+        fclose(file);
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        fprintf(stderr, "Error: Error during png read\n");
+        return NULL;
+    }
+
+    // Initialize PNG IO
+    png_init_io(png_ptr, file);
+    png_set_sig_bytes(png_ptr, 8);
+
+    // Read PNG info
+    png_read_info(png_ptr, info_ptr);
+    int width = png_get_image_width(png_ptr, info_ptr);
+    int height = png_get_image_height(png_ptr, info_ptr);
+    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
+    png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+    // Read any color_type to 8-bit depth, RGBA format.
+    // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+    if (bit_depth == 16)
+        png_set_strip_16(png_ptr);
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png_ptr);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png_ptr);
+
+    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png_ptr);
+
+    if (color_type == PNG_COLOR_TYPE_RGB ||
+        color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png_ptr);
+
+    // Update the png info struct.
+    png_read_update_info(png_ptr, info_ptr);
+
+    // Allocate memory for image data
+    png_bytep *row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
+    for (int y = 0; y < height; y++)
+    {
+        row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
+    }
+
+    // Read the image data
+    png_read_image(png_ptr, row_pointers);
+
+    // Create an SDL surface
+    SDL_Surface *surface = SDL_CreateRGBSurface(0, width, height, 32,
+                                                0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+
+    // Copy pixel data from PNG to SDL surface
+    for (int y = 0; y < height; y++)
+    {
+        png_bytep row = row_pointers[y];
+        for (int x = 0; x < width; x++)
+        {
+            png_bytep px = &(row[x * 4]);
+            Uint32 color = ((px[0] << 24) | (px[1] << 16) | (px[2] << 8) | px[3]);
+            putpixel(surface, x, y, color);
+        }
+    }
+
+    // Free libpng allocated memory
+    for (int y = 0; y < height; y++)
+    {
+        free(row_pointers[y]);
+    }
+    free(row_pointers);
+
+    // Clean up libpng structures
+    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+    fclose(file);
+
+    return surface;
+}
 
 SDL_Surface *LoadSurfaceFromFile(const char *filePath, void **data)
 {
-    int req_format = STBI_rgb_alpha;
-    int width, height, orig_format;
-    *data = stbi_load(filePath, &width, &height, &orig_format, req_format);
-#ifdef __EMSCRIPTEN__
-    flipImageVertically((char *)(*data), width, height, orig_format);
-#endif
-    if (*data == NULL)
-    {
-        SDL_Log("Loading image failed: %s", stbi_failure_reason());
-        exit(1);
-    }
-
-    int depth, pitch;
-    Uint32 pixel_format;
-    if (req_format == STBI_rgb)
-    {
-        depth = 24;
-        pitch = 3 * width; // 3 bytes per pixel * pixels per row
-        pixel_format = SDL_PIXELFORMAT_RGB24;
-    }
-    else
-    { // STBI_rgb_alpha (RGBA)
-        depth = 32;
-        pitch = 4 * width;
-        pixel_format = SDL_PIXELFORMAT_RGBA32;
-    }
-
-    SDL_Surface *surf = SDL_CreateRGBSurfaceWithFormatFrom((void *)*data, width, height,
-                                                           depth, pitch, pixel_format);
-
-    if (surf == NULL)
-    {
-        SDL_Log("Creating surface failed: %s", SDL_GetError());
-        stbi_image_free(*data);
-        exit(1);
-    }
-
-    if (!surf)
-    {
-        LogError("Could not create surface from data %s, Error:\n%s", filePath, SDL_GetError());
-        return NULL;
-    }
-    // stbi_image_free(data);
+    SDL_Surface *surf = loadPNG(filePath);
     return surf;
+
+    //     int req_format = STBI_rgb_alpha;
+    //     int width, height, orig_format;
+    //     *data = stbi_load(filePath, &width, &height, &orig_format, req_format);
+    // #ifdef __EMSCRIPTEN__
+    //     flipImageVertically((char *)(*data), width, height, orig_format);
+    // #endif
+    //     if (*data == NULL)
+    //     {
+    //         SDL_Log("Loading image failed: %s", stbi_failure_reason());
+    //         exit(1);
+    //     }
+
+    //     int depth, pitch;
+    //     Uint32 pixel_format;
+    //     if (req_format == STBI_rgb)
+    //     {
+    //         depth = 24;
+    //         pitch = 3 * width; // 3 bytes per pixel * pixels per row
+    //         pixel_format = SDL_PIXELFORMAT_RGB24;
+    //     }
+    //     else
+    //     { // STBI_rgb_alpha (RGBA)
+    //         depth = 32;
+    //         pitch = 4 * width;
+    //         pixel_format = SDL_PIXELFORMAT_RGBA32;
+    //     }
+
+    //     SDL_Surface *surf = SDL_CreateRGBSurfaceWithFormatFrom((void *)*data, width, height,
+    //                                                            depth, pitch, pixel_format);
+
+    //     if (surf == NULL)
+    //     {
+    //         SDL_Log("Creating surface failed: %s", SDL_GetError());
+    //         stbi_image_free(*data);
+    //         exit(1);
+    //     }
+
+    //     if (!surf)
+    //     {
+    //         LogError("Could not create surface from data %s, Error:\n%s", filePath, SDL_GetError());
+    //         return NULL;
+    //     }
+    //     // stbi_image_free(data);
+    //     return surf;
 }
 void DestroyPixelData(void *data)
 {
-    stbi_image_free(data);
+    // stbi_image_free(data);
 }
 
 SDL_Surface *LoadTextureAtlas(int width, int height)
@@ -118,7 +250,7 @@ SDL_Texture *CreateTextureFromFile(const char *filename)
     void *data = NULL;
     SDL_Surface *surface = LoadSurfaceFromFile(filename, &data);
     SDL_Texture *texture = CreateTextureFromSurface(surface);
-    stbi_image_free(data);
+    // stbi_image_free(data);
     return texture;
 }
 void DestroySurface(SDL_Surface *surface)
@@ -149,7 +281,7 @@ void DrawTexture(SDL_Texture *texture, geRectangle *srcRect, geRectangle *dstRec
 
     SDL_Rect translatedDstRect;
     // translatedDstRect.x = g_backgroundDrawRect->x - dstRect->x;
-    translatedDstRect.x =  dstRect->x - g_backgroundDrawRect->x;
+    translatedDstRect.x = dstRect->x - g_backgroundDrawRect->x;
     // translatedDstRect.y = g_backgroundDrawRect->y - dstRect->y;
     translatedDstRect.y = dstRect->y;
     translatedDstRect.w = dstRect->w;
@@ -157,7 +289,7 @@ void DrawTexture(SDL_Texture *texture, geRectangle *srcRect, geRectangle *dstRec
     // SDL_RenderCopy(g_pRenderer, texture, srcRect, dstRect);
     SDL_RenderCopyEx(g_pRenderer,
                      texture,
-                     (SDL_Rect*)srcRect,
+                     (SDL_Rect *)srcRect,
                      &translatedDstRect,
                      0,
                      NULL,
@@ -165,11 +297,11 @@ void DrawTexture(SDL_Texture *texture, geRectangle *srcRect, geRectangle *dstRec
     //  (shouldFlip) ? SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL : SDL_FLIP_NONE | SDL_FLIP_VERTICAL);
 }
 
-void geDrawDebugRect(geRectangle* dstRect, geColor* color)
+void geDrawDebugRect(geRectangle *dstRect, geColor *color)
 {
     SDL_Rect translatedDstRect;
     // translatedDstRect.x = g_backgroundDrawRect->x - dstRect->x;
-    translatedDstRect.x =  dstRect->x - g_backgroundDrawRect->x;
+    translatedDstRect.x = dstRect->x - g_backgroundDrawRect->x;
     // translatedDstRect.y = g_backgroundDrawRect->y - dstRect->y;
     translatedDstRect.y = dstRect->y;
     translatedDstRect.w = dstRect->w;
