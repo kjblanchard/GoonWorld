071885685e5492b169510ebf0f653404f3df3c81
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-14 20:31:37 -0400

    Set proper for best viewing

--- external/sg_engine/src/GoonEngine/scripting/SdlWindow.c
+++ external/sg_engine/src/GoonEngine/scripting/SdlWindow.c
@@ -20,18 +20,16 @@ int geInitializeRenderingWindow(unsigned int windowWidth, unsigned int windowHei
 {
     g_pWindow = SDL_CreateWindow(windowName,
                                  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                 //  width, height,
                                  windowWidth,
                                  windowHeight,
                                  SDL_WINDOW_SHOWN);
-    // if the window creation succeeded create our renderer
     if (g_pWindow == NULL)
     {
         fprintf(stderr, "Window could not be created, Error: %s", SDL_GetError());
         return 0;
     }
-    // g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_ACCELERATED);
+    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, SDL_RENDERER_ACCELERATED);
     if (g_pRenderer == NULL)
     {
         fprintf(stderr, "Renderer could not be created, Error: %s", SDL_GetError());
@@ -39,15 +37,12 @@ int geInitializeRenderingWindow(unsigned int windowWidth, unsigned int windowHei
     fprintf(stdout, "Created window\nWidth: %d, Height: %d", windowWidth, windowHeight);
     SDL_DisplayMode mode;
     SDL_GetWindowDisplayMode(g_pWindow, &mode);
-    g_refreshRate = mode.refresh_rate ? mode.refresh_rate : 60;
     // g_refreshRate = 30;
-    LogDebug("Refresh rate is set to %d", g_refreshRate);
     // g_refreshRate = 120;
+    g_refreshRate = mode.refresh_rate ? mode.refresh_rate : 60;
+    LogDebug("Refresh rate is set to %d", g_refreshRate);
     SDL_RenderSetIntegerScale(g_pRenderer, SDL_TRUE);
-    scaleX = windowWidth / gameWidth;
-    scaleY = windowHeight / gameHeight;
-    SDL_RenderSetScale(g_pRenderer, scaleX, scaleY);
-    // SDL_RenderSetLogicalSize(g_pRenderer, 2048, 1192);
+    SDL_RenderSetLogicalSize(g_pRenderer, gameWidth, gameHeight);
     return 0;
 }
 
